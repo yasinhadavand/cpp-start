607f8706dfbbdc26aaafac27ed1b87e34c12ca34
yasin <yasintexac@gmail.com>
2023-05-25 23:12:20 +0300

    minimum/maximum number / recursion function

---
+++ recursion9.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int maximumNumber(int, int ,int);
+
+int main(void){
+	int num1,num2,num3;
+	cout<<"please enter value of x : ";
+	cin>>num1;
+	cout<<"please enter value of y : ";
+	cin>>num2;
+	cout<<"please enter value of z : ";
+	cin>>num3;
+	cout<<"maxnum number is : "<<maximumNumber(num1,num2,num3);
+	cout<<"minimum number is : "<<minimumNumber(num1,num2,num3);
+	return 0;
+}
+int maximumNumber(int a, int b,int c){
+//	int result;
+	if(a>b && a>c){
+		return a;
+			
+	}else if(b>a && b>c){
+		
+		return b;
+		
+	}else if(c>a && c>b){
+		
+		return c;
+	}
+	
+}
+int minimumNumber(int d, int f,int g){
+//	int result;
+	if(d<f && d<g){
+		return d;
+			
+	}else if(f<d && f<g){
+		
+		return f;
+		
+	}else if(g<d && g<f){
+		
+		return g;
+	}
+	
+}
