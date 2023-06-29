580da920f17c8c04481b4050fb9fdea49e4f39ea
yasin <yasintexac@gmail.com>
2023-06-29 08:55:22 +0300

    the number want to add

--- array3.cpp
+++ array3.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 
 int main(void){
-    int num[10],i;
-    for (int i = 0; i < 10 ; i++)
+    int count[10],i,number;
+    cout<<"How many number you want to sum ? ";
+    cin>>number;
+    for (int i = 0; i < number ; i++)
     {
         /* code */
         cout<<"enter number "<<i+1<<" : ";
-        cin>>num[i];
+        cin>>count[i];
     }
     int sum=0;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < number; i++)
     {
         /* code */
-        sum = sum + num[i];
+        sum = sum + count[i];
 
     }
     cout<<sum;
