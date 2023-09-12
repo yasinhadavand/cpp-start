f2b25e6a3d914ee4683bbaf1dc39601098c7f035
Yasin Hadavand <yasintexac@gmail.com>
2023-09-12 15:53:04 +0300

    Process Transaction

--- bank.cpp
+++ bank.cpp
@@ -43,6 +43,84 @@ public:
     }
 };
 
+class Bank
+{
+private:
+    vector<Account>accounts;
+    int nextAccountNumber;
+public:
+    Bank(/* args */);{
+    nextAccountNumber = 1;    
+    }
+    
+    void createAccount(string accountHolderName, double initialBalance){
+        Account newAccount(nextAccountNumber, accountHolderName, initialBalanc);
+        accounts.push_back(newAccount);
+        nextAccountNumber++;
+        cout<< "Account create seccessfully!"<<endl;
+    }
+    Account* findAccount(int accountNum) {
+        for (size_t i = 0; i < accounts.size(); i++)
+        {
+            /* code */
+            if (accounts[i].getAccountNumber() == accountNumber)
+            {
+                /* code */
+                return &accounts[i];
+            }
+            return nullptr;
+        }
+        
+    }
+    void processTransaction(){
+        int accountNumber;
+        cout<<"Enter Account Number: ";
+        cin>>accountNumber;
+
+
+        Account* acc = findAccount(accountNumber);
+
+        if (acc != nullptr)
+        {
+            /* code */
+            int choice;
+            cout<<"1: Deposit"<<endl;
+            cout<<"2: Withdraw"<<endl;
+            cout<<"3: Display Balance"<<endl;
+            cout<<"Enter your choice : ";
+            cin<<choice;
+
+            switch (choice)
+            {
+            case 1:{
+                double amount;
+                cout<<"Enter the deposite amount: $";
+                cin>>amount;
+                acc->deposit(amount);
+                break;
+            }
+            case 2:{
+                double amount;
+                cout<<"Enter the withdrawal amount: $"
+                cin>>amount;
+                acc->withdraw(amount);
+                break;
+            }
+            case 3:{
+                acc->displayBalance();
+                break;
+            }
+            
+            default:
+                cout<<"invalid choice"<<endll;
+            }
+        }else{
+            cout<<"Account not fount."<<endl;
+        }
+        
+    }
+};
+
 
 
 
