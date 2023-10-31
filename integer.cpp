4493d9ee203824521a0aff08e49a239b76264cde
Yasin Hadavand <yasintexac@gmail.com>
2023-10-31 12:59:13 +0200

    Get integer number from user

---
+++ integer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int m, n;
+	char a;
+	cout<<"Enter two integer";
+	cin>>m>>n;
+	cout<<"m="<<m<<" n="<<n<<endl;
+	cout<<"Enter a character";
+	cin>>a;
+	cout<<"Character is "<<a<<endl;
+	return 0;
+}
+
