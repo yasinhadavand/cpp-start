5d683f2bd6117ef33c84f9705e9f0bfa9e01fbdc
Yasin Hadavand <yasintexac@gmail.com>
2023-11-01 22:45:09 +0200

    sqrt with range number

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,10 +1,28 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
-int main(void){
+int main(void)
+{
+    int x, y;
+    // for (int i = 0; i <= 5; i++)
+    // {
+    //     cout << i << " " << sqrt(i) << endl;
+    // };
+    cout << "Enter number range that you want Sqrt : " << endl;
+    cin >> x >> y;
 
-	for(int i = 0 ; i <= 5; i++){
-		cout << i << " " << sqrt(i) <<endl;
-	}
+    if (y > x)
+    {
+        for (int i = x; i <= y; i++)
+        {
+            cout << i << " = " << sqrt(i) << endl;
+        }
+    }else{
+        for (int i = y; i <= x; i++)
+        {
+            cout << i << " = " <<sqrt(i)<<endl;
+        }
+        
+    }
 }
