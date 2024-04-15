084965032a6be6ea0d896710ded21f5a729d6a83
Yasin Hadavand <yasintexac@gmail.com>
2024-04-15 11:19:00 +0300

    declared stack

--- CMPE242.cpp
+++ CMPE242.cpp
@@ -1,27 +1,52 @@
+// #include <iostream>
+// #define SIZE 3
+// using namespace std;
+
+// int main(void){
+//     struct q2
+//     {
+//         int age;
+//         char name[20];
+//     }s[SIZE];
+
+//     for (int i = 0; i < SIZE; i++)
+//     {
+//         cout<<"Enter age student "<<i+1<<" :";
+//         cin>>s[i].age;
+//         cout<<"Enter name sudent "<<i+1<<" :";
+//         cin>>s[i].name;
+
+//     }
+//     for (int i = 0; i < SIZE; i++)
+    
+//         cout<<s[i].name <<"\t"<<s[i].age<<endl;
+    
+//     return 0;
+    
+// }
 #include <iostream>
-#define SIZE 3
-using namespace std;
+#define STACKSIZE 100
+
+struct stack
+{
+    int top;
+    int items[STACKSIZE];
+};
 
 int main(void){
-    struct q2
-    {
-        int age;
-        char name[20];
-    }s[SIZE];
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        cout<<"Enter age student "<<i+1<<" :";
-        cin>>s[i].age;
-        cout<<"Enter name sudent "<<i+1<<" :";
-        cin>>s[i].name;
-
-    }
-    for (int i = 0; i < SIZE; i++)
-    
-        cout<<s[i].name <<"\t"<<s[i].age<<endl;
-    
-    
-    return 0;
-    
+    struct stack s;
+    return 0 ;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
